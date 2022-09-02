defc064d774f178439a1891d654fcbfccf58ca15
PugaMarcoIT <100373746+PugaMarcoIT@users.noreply.github.com>
2022-09-02 16:22:28 -0500

    Add files via upload

---
+++ prueba.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+#include <stdio.h>
+#include <conio.h>
+float resultado;
+int a,b,c,d,a,b,c,d;
+char opcion;
+float radio, altura, x;
+// Este programa calcula el volumen de un cilindro.
+void main()
+{
+    a = (3 + 5)*8 - (10 - 4)/2;
+}
